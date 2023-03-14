9ac005466fc52d4e6ed816b5e46e18220fbed754
Italo Teles <italop.teles@gmail.com>
2023-03-13 21:00:15 -0300

    Formulários reativos de perfil

--- Front/ProEventos-App/src/app/components/user/perfil/perfil.component.ts
+++ Front/ProEventos-App/src/app/components/user/perfil/perfil.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControlOptions, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ValidatorField } from '@app/helpers/ValidatorField';
 
 @Component({
   selector: 'app-perfil',
@@ -7,9 +9,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PerfilComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
 
-  ngOnInit() {
+  get f() : any{
+    return this.form.controls;
   }
 
+  constructor(private formBuilder : FormBuilder) { }
+
+  ngOnInit() {
+    this.validation();
+  }
+
+  public validation(): void {
+
+    const formOptions : AbstractControlOptions = {
+      validators : ValidatorField.MustMatch('senha', 'confirmarSenha')
+    };
+
+    this.form = this.formBuilder.group({
+
+      titulo: ["", Validators.required],
+      primeiroNome: ["", Validators.required],
+      ultimoNome: ["", Validators.required],
+      email: ["", [Validators.required, Validators.email]],
+      telefone: ["", Validators.required],
+      funcao: ["", Validators.required],
+      descricao: ["", Validators.required],
+      senha: ["", Validators.required],
+      confirmarSenha: ["", Validators.required],
+
+    },formOptions);
+  }
+  public resetForm(evento : any){
+    evento.preventDefault();
+    this.form.reset();
+  }
+
+  public onSubmit() : void{
+
+    //vai parar aqui se form estiver inválido
+    if (this.form.invalid){
+      return;
+    }
+  }
 }
