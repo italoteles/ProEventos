3dff72596955550a00bdf095325413a1fc8dfd14
Italo Teles <italop.teles@gmail.com>
2023-04-25 21:09:30 -0300

    Finalização paginação backend

--- Back/src/ProEventos.API/Controllers/EventosController.cs
+++ Back/src/ProEventos.API/Controllers/EventosController.cs
@@ -5,6 +5,7 @@ using ProEventos.Application.Contratos;
 using ProEventos.Application.Dtos;
 using ProEventos.API.Extensions;
 using Microsoft.AspNetCore.Authorization;
+using ProEventos.Persistence.Models;
 
 namespace ProEventos.API.Controllers;
 
@@ -15,7 +16,7 @@ public class EventosController : ControllerBase
 {
     private IEventoService _eventoService;
     private IWebHostEnvironment _hostEnvironment;
-        public IAccountService _accountService { get; }
+    public IAccountService _accountService { get; }
 
     public EventosController(IEventoService eventoService, IWebHostEnvironment hostEnvironment, IAccountService accountService)
     {
@@ -26,29 +27,29 @@ public class EventosController : ControllerBase
     }
 
     [HttpGet]
-    public async Task<IActionResult> GetAll()
+    public async Task<IActionResult> Get([FromQuery] PageParams pageParams)
     {
         try
         {
-            var eventos = await _eventoService.GetAllEventosAsync(User.GetUserId(), true);
+            var eventos = await _eventoService.GetAllEventosAsync(User.GetUserId(), pageParams, true);
             if (eventos == null) return NoContent();
 
-    
+            Response.AddPagination(eventos.CurrentPage, eventos.PageSize, eventos.TotalCount, eventos.TotalPages);
+
             return Ok(eventos);
         }
         catch (Exception ex)
         {
-
-            return this.StatusCode(StatusCodes.Status500InternalServerError, $"Erro ao tentar recuperar eventos. Erro: {ex.Message}");
+            return this.StatusCode(StatusCodes.Status500InternalServerError,
+                $"Erro ao tentar recuperar eventos. Erro: {ex.Message}");
         }
-
     }
     [HttpGet("{id}")]
     public async Task<IActionResult> GetById(int id)
     {
         try
         {
-            var evento = await _eventoService.GetAEventoByIdAsync(User.GetUserId(),id, true);
+            var evento = await _eventoService.GetAEventoByIdAsync(User.GetUserId(), id, true);
             if (evento == null) return NoContent();
             return Ok(evento);
         }
@@ -61,29 +62,14 @@ public class EventosController : ControllerBase
 
 
     }
-    [HttpGet("{tema}/tema")]
-    public async Task<IActionResult> GetByTema(string tema)
-    {
-        try
-        {
-            var evento = await _eventoService.GetAllEventosByTemaAsync(User.GetUserId(),tema, true);
-            if (evento == null) return NoContent();
-            return Ok(evento);
-        }
-        catch (Exception ex)
-        {
-
-            return this.StatusCode(StatusCodes.Status500InternalServerError, $"Erro ao tentar recuperar o evento com este tema. Erro: {ex.Message}");
-        }
-
-    }
+    
 
     [HttpPost]
     public async Task<IActionResult> Post(EventoDto model)
     {
         try
         {
-            var evento = await _eventoService.AddEvento(User.GetUserId(),model);
+            var evento = await _eventoService.AddEvento(User.GetUserId(), model);
             if (evento == null) return BadRequest("Erro ao tentar adicionar o evento!");
             return Ok(evento);
         }
@@ -99,16 +85,17 @@ public class EventosController : ControllerBase
     {
         try
         {
-            var evento = await _eventoService.GetAEventoByIdAsync(User.GetUserId(),eventoId, true);
+            var evento = await _eventoService.GetAEventoByIdAsync(User.GetUserId(), eventoId, true);
             if (evento == null) return NoContent();
 
             var file = Request.Form.Files[0];
-            if (file.Length > 0){
+            if (file.Length > 0)
+            {
                 DeleteImage(evento.ImagemURL);
                 evento.ImagemURL = await SaveImage(file);
             }
 
-            var EventoRetorno = await _eventoService.UpdateEvento(User.GetUserId(),eventoId, evento);
+            var EventoRetorno = await _eventoService.UpdateEvento(User.GetUserId(), eventoId, evento);
             return Ok(EventoRetorno);
         }
         catch (Exception ex)
@@ -124,7 +111,7 @@ public class EventosController : ControllerBase
     {
         try
         {
-            var evento = await _eventoService.UpdateEvento(User.GetUserId(),id, model);
+            var evento = await _eventoService.UpdateEvento(User.GetUserId(), id, model);
             if (evento == null) return NoContent();
             return Ok(evento);
         }
@@ -141,17 +128,20 @@ public class EventosController : ControllerBase
     {
         try
         {
-            var evento = await _eventoService.GetAEventoByIdAsync(User.GetUserId(),id, true);
+            var evento = await _eventoService.GetAEventoByIdAsync(User.GetUserId(), id, true);
             if (evento == null) return NoContent();
 
 
-            if (await _eventoService.DeleteEvento(User.GetUserId(),id)){
+            if (await _eventoService.DeleteEvento(User.GetUserId(), id))
+            {
                 DeleteImage(evento.ImagemURL);
-                return Ok(new {message = "Deletado" }); 
+                return Ok(new { message = "Deletado" });
 
-            } else{
+            }
+            else
+            {
 
-                 throw new Exception("Ocorreu um erro ao deletar evento.");
+                throw new Exception("Ocorreu um erro ao deletar evento.");
             }
 
         }
@@ -164,22 +154,25 @@ public class EventosController : ControllerBase
     }
 
     [NonAction]
-    public async Task<string> SaveImage(IFormFile   imageFile){
-       
-        string imageName = new string(Path.GetFileNameWithoutExtension(imageFile.FileName).Take(10).ToArray()).Replace(' ','-');
+    public async Task<string> SaveImage(IFormFile imageFile)
+    {
+
+        string imageName = new string(Path.GetFileNameWithoutExtension(imageFile.FileName).Take(10).ToArray()).Replace(' ', '-');
 
         imageName = $"{imageName}{DateTime.UtcNow.ToString("yymmssfff")}{Path.GetExtension(imageFile.FileName)}";
 
         var imagePath = Path.Combine(_hostEnvironment.ContentRootPath, @"Resourcers/images", imageName);
 
-        using (var fileStream = new FileStream(imagePath, FileMode.Create)){
+        using (var fileStream = new FileStream(imagePath, FileMode.Create))
+        {
             await imageFile.CopyToAsync(fileStream);
         }
 
-       return imageName;
+        return imageName;
     }
     [NonAction]
-    public void DeleteImage(string imageName){
+    public void DeleteImage(string imageName)
+    {
         var imagePath = Path.Combine(_hostEnvironment.ContentRootPath, @"Resourcers/images", imageName);
         if (System.IO.File.Exists(imagePath))
             System.IO.File.Delete(imagePath);
--- Back/src/ProEventos.Application/EventoService.cs
+++ Back/src/ProEventos.Application/EventoService.cs
@@ -3,6 +3,7 @@ using ProEventos.Application.Contratos;
 using ProEventos.Application.Dtos;
 using ProEventos.Domain;
 using ProEventos.Persistence.Contratos;
+using ProEventos.Persistence.Models;
 
 namespace ProEventos.Application
 {
@@ -26,27 +27,29 @@ namespace ProEventos.Application
                 evento.UserId = userId;
 
                 _geralPersist.Add<Evento>(evento);
-                if (await _geralPersist.SaveChangesAsync()){
-                    var eventoRetorno = await _eventoPersist.GetAEventoByIdAsync(userId,evento.Id, false);
+                if (await _geralPersist.SaveChangesAsync())
+                {
+                    var eventoRetorno = await _eventoPersist.GetAEventoByIdAsync(userId, evento.Id, false);
                     return _mapper.Map<EventoDto>(eventoRetorno);
                 }
-            
+
                 return null;
             }
             catch (System.Exception ex)
             {
                 throw new Exception(ex.Message);
-                
+
             }
         }
         public async Task<EventoDto> UpdateEvento(int userId, int eventoId, EventoDto model)
         {
             try
             {
-                
+
 
                 var evento = await _eventoPersist.GetAEventoByIdAsync(userId, eventoId, false);
-                if (evento ==null){
+                if (evento == null)
+                {
                     return null;
                 }
 
@@ -57,80 +60,69 @@ namespace ProEventos.Application
 
                 _geralPersist.Update<Evento>(evento);
 
-                if (await _geralPersist.SaveChangesAsync()){
-                    var eventoRetorno = await _eventoPersist.GetAEventoByIdAsync(userId,evento.Id, false);
-                    
+                if (await _geralPersist.SaveChangesAsync())
+                {
+                    var eventoRetorno = await _eventoPersist.GetAEventoByIdAsync(userId, evento.Id, false);
+
                     return _mapper.Map<EventoDto>(eventoRetorno);
                 }
                 return null;
             }
             catch (Exception ex)
             {
-                
+
                 throw new Exception(ex.Message);
             }
         }
 
-        public async Task<bool> DeleteEvento(int userId,  int eventoId)
+        public async Task<bool> DeleteEvento(int userId, int eventoId)
         {
             try
             {
-                var evento = await _eventoPersist.GetAEventoByIdAsync(userId,eventoId, false);
-                if (evento ==null){
+                var evento = await _eventoPersist.GetAEventoByIdAsync(userId, eventoId, false);
+                if (evento == null)
+                {
                     throw new Exception("Evento para delete não foi encontrado");
                 }
 
                 _geralPersist.Delete<Evento>(evento);
 
                 return await _geralPersist.SaveChangesAsync();
-                   
-                
+
+
             }
             catch (Exception ex)
             {
-                
+
                 throw new Exception(ex.Message);
             }
         }
 
-        public async Task<EventoDto[]> GetAllEventosAsync(int userId, bool includePalestrante = false)
+        public async Task<PageList<EventoDto>> GetAllEventosAsync(int userId, PageParams pageParams, bool includePalestrante = false)
         {
             try
             {
-                var eventos = await _eventoPersist.GetAllEventosAsync(userId,includePalestrante);
+                var eventos = await _eventoPersist.GetAllEventosAsync(userId, pageParams, includePalestrante);
                 if (eventos == null) return null;
 
-                var resultado = _mapper.Map<EventoDto[]>(eventos);
-            
+                var resultado = _mapper.Map<PageList<EventoDto>>(eventos);
 
+                resultado.CurrentPage = eventos.CurrentPage;
+                resultado.TotalPages = eventos.TotalPages;
+                resultado.PageSize = eventos.PageSize;
+                resultado.TotalCount = eventos.TotalCount;
+                
                 return resultado;
 
-                
+
             }
             catch (Exception ex)
             {
-                
-                 throw new Exception(ex.Message);
+
+                throw new Exception(ex.Message);
             }
         }
-        public async Task<EventoDto[]> GetAllEventosByTemaAsync(int userId, string tema, bool includePalestrante = false)
-        {
-            try
-            {
-                var eventos = await _eventoPersist.GetAllEventosByTemaAsync(userId,tema, includePalestrante);
-                if (eventos == null) return null;
 
-                var resultado = _mapper.Map<EventoDto[]>(eventos);
-            
-
-                return resultado;
-            }
-            catch (Exception ex)
-            {
-                
-                 throw new Exception(ex.Message);
-            }
-        }
         public async Task<EventoDto> GetAEventoByIdAsync(int userId, int eventoId, bool includePalestrante = false)
         {
             try
@@ -143,8 +135,8 @@ namespace ProEventos.Application
             }
             catch (Exception ex)
             {
-                
-                 throw new Exception(ex.Message);
+
+                throw new Exception(ex.Message);
             }
         }
 
